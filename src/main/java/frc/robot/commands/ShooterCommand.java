2169276225d92f69f7a2511318638139e1603e17
epicmonky <akash20952@gmail.com>
2021-02-13 09:49:20 -0500

    Added shooter subsystem and command

---
+++ src/main/java/frc/robot/commands/ShooterCommand.java
@@ -0,0 +1,46 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.ShooterSubsystem;
+
+public class ShooterCommand extends CommandBase {
+
+  private final ShooterSubsystem m_shooterSubsystem;
+  private final DoubleSupplier m_input;
+
+  /** Creates a new ShooterCommand. */
+  public ShooterCommand(DoubleSupplier input, ShooterSubsystem shooterSubsystem) {
+    m_shooterSubsystem = shooterSubsystem;
+    m_input = input;
+
+    addRequirements(m_shooterSubsystem);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    m_shooterSubsystem.shoot(m_input.getAsDouble());
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    m_shooterSubsystem.stop();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
