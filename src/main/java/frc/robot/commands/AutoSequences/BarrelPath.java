603ebef94b593fc27e13fe04fbb4cd1dca54b874
John Lee <2003jjl@gmail.com>
2021-04-03 10:45:23 -0400

    little restructuring

--- src/main/java/frc/robot/commands/AutoSequences/BarrelPath.java
+++ src/main/java/frc/robot/commands/AutoSequences/BarrelPath.java
@@ -2,9 +2,12 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands;
+package frc.robot.commands.AutoSequences;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.BangDrive;
+import frc.robot.commands.CCWArc;
+import frc.robot.commands.CWArc;
 import frc.robot.subsystems.DriveTrain;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
